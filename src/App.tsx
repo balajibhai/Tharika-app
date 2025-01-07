d9ba3b3fc01133c19d3966753433a4388d4afbde
hari <codering142@gmail.com>
2025-01-07 00:56:35 -0500

    router changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import AfterAuth from "./Molecules/AfterAuth";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useAppSelector } from "./Hooks/customhooks";
+import AfterAuth from "./Molecules/AfterAuth";
 import AuthPage from "./Organisms/AuthPage";
 
 const App = () => {
   const { loginState } = useAppSelector((state) => state.authenticationReducer);
 
-  return !loginState ? <AuthPage /> : <AfterAuth />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={loginState ? <Navigate to="/home" replace /> : <AuthPage />}
+        />
+        <Route
+          path="/home"
+          element={loginState ? <AfterAuth /> : <Navigate to="/" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/Atoms/Button.tsx
+++ src/Atoms/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface ButtonProps {
   children: React.ReactNode;
   onClick: () => void;
-  style: {};
+  style: React.CSSProperties;
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick, style }) => {
